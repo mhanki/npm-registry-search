370e110ba5092ca4a333a43572c7ef9496e7c4f5
mhanki <meike.hankewicz@protonmail.com>
2022-08-15 11:49:09 +0200

    Add data rendering

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -11,12 +11,15 @@ const RepositoriesList: React.FC = () => {
     event.preventDefault();
     searchRepositories(term);
   }
-  
+
   return <div>
     <form onSubmit={onSubmit}>
       <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
       <button>Search</button>
     </form>
+    {error && <h3>{error}</h3>}
+    {loading && <h3>Loading...</h3>}
+    {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
   </div>
 }
 
