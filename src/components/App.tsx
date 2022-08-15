d10f3401efb48e9a78f7bc73b79677a6073c4562
mhanki <meike.hankewicz@protonmail.com>
2022-08-15 11:21:57 +0200

    Add redux state to the React app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,14 @@
-const App = () => <div>Hello there!</div>;
+import { Provider } from 'react-redux';
+import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
+
+const App = () => {
+  return <Provider store={store}>
+    <div>
+      <h1>Search For A Package</h1>
+      <RepositoriesList />
+    </div>
+  </Provider>
+};
 
 export default App;
